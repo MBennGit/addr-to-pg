266fbd06feb968dc94858e3ddfa87d0a19143ae2
M.Benninghoff <mb.social@posteo.de>
2021-12-23 00:44:00 +0200

    improved geoapify function

--- app/src/geoapify.py
+++ app/src/geoapify.py
@@ -2,7 +2,15 @@ import requests
 from requests.structures import CaseInsensitiveDict
 from shapely.geometry import Point, shape
 
-from config import geoapikey
+from config import GEOAPIFYKEY, VALID_MATCH_TYPES
+
+
+class LowConfidence(Exception):
+    pass
+
+
+class NoResultsReturned(Exception):
+    pass
 
 
 baseurl = "https://api.geoapify.com/v1/geocode/search"
@@ -11,8 +19,13 @@ headers = CaseInsensitiveDict()
 headers["Accept"] = "application/json"
 
 
-def get_point_from_address(address: str = '', api_key: str = geoapikey, url: str = baseurl) -> Point:
+def get_point_from_address(address: str = '', api_key: str = GEOAPIFYKEY, url: str = baseurl) -> Point:
     payload = {'text': address, 'apiKey': api_key}
     resp = requests.get(url, headers=headers, params=payload)
+    d = resp.json()
+    if len(d['features']) == 0:
+        raise NoResultsReturned
+    f1 = d['features'][0]
+    if f1['properties']['rank']['match_type'] in VALID_MATCH_TYPES:
+        raise LowConfidence
     return shape(resp.json()['features'][0]['geometry'])
-
---
+++ config.py
@@ -0,0 +1,2 @@
+GEOAPIFYKEY = ""
+VALID_MATCH_TYPES = ['inner_part', 'match_by_street']
